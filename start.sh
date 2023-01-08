239bad547d0ad941b62d183b710d5a3ee6b66902
dungbkhn <86149640+dungbkhn@users.noreply.github.com>
2023-01-08 18:56:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -30,14 +30,16 @@ cd ./make_sftp_restrict_folder
 chmod 755 make_sftp_restrict_folder.sh
 chmod 755 install_com.sh
 ./make_sftp_restrict_folder.sh
+chmod 755 setup_for_crontab.sh
+chmod 755 setup_for_root_crontab.sh
 cat ../ssf/crontab_header.txt > crontabtmpfile
-echo "@reboot /home/${p1}/make_sftp_restrict_folder/setup_for_crontab ${p1}" >> crontabtmpfile
+echo "@reboot /home/${p1}/make_sftp_restrict_folder/setup_for_crontab.sh ${p1}" >> crontabtmpfile
 cp crontabtmpfile /var/spool/cron/crontabs/dungnt
 chgrp crontab /var/spool/cron/crontabs/dungnt
 chown dungnt /var/spool/cron/crontabs/dungnt
 rm crontabtmpfile
 cat ../ssf/crontab_header.txt > crontabtmpfile
-echo "@reboot /home/${p1}/make_sftp_restrict_folder/setup_for_root_crontab ${p1}" >> crontabtmpfile
+echo "@reboot /home/${p1}/make_sftp_restrict_folder/setup_for_root_crontab.sh ${p1}" >> crontabtmpfile
 cp crontabtmpfile /var/spool/cron/crontabs/root
 chgrp crontab /var/spool/cron/crontabs/root
 chown root /var/spool/cron/crontabs/root
